cf587465623a5bde938bb990ac53180f45ef8ec5
koralle <koralle@koralle-macbook-air.local>
2025-09-12 16:15:41 +0900

    feat(prettier): write `.prettierrc.mjs`

---
+++ .prettierrc.mjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+export default {
+  bracketSameLine: false,
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  singleAttributePerLine: true,
+  trailingComma: 'none'
+};
